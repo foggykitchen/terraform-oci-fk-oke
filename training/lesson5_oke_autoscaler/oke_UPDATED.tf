8d60394be1494aec7b1c415bc407c3b472bb3b80
mlinxfeld <martin.linxfeld@foggykitchen.com>
2026-05-25 13:05:25 +0200

    Modernize lesson5 autoscaler example

--- training/lesson5_oke_autoscaler/oke_UPDATED.tf
+++ training/lesson5_oke_autoscaler/oke_UPDATED.tf
@@ -1,6 +1,6 @@
 module "fk-oke" {
   providers                             = { oci = oci.targetregion }
-  source                                = "github.com/foggykitchen/terraform-oci-fk-oke"
+  source                                = "../.."
   tenancy_ocid                          = var.tenancy_ocid
   region                                = var.region 
   compartment_ocid                      = var.compartment_ocid
